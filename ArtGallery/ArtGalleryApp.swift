85d43098c9c97fded90039b36723821d9742f33a
arzaluz-chris <23008381@es.uveg.edu.mx>
2026-06-08 11:03:06 -0600

    Initial Commit

---
+++ ArtGallery/ArtGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtGalleryApp.swift
+//  ArtGallery
+//
+//  Created by Christian Arzaluz on 08/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ArtGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
